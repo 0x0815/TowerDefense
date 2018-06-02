62fb8d303d0b53c9d070b6ef85db887c11bf0738
0x0815 <32913299+0x0815@users.noreply.github.com>
2018-06-02 02:30:01 +0200

    Added Map and Enemy
    Added Map and first Enemy.
    Added Waypoints.

---
+++ TowerDefense/Assets/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float speed = 10f;
+    private Transform target;
+    private int wayPointIndex;
+
+    private void Start()
+    {
+        target = Waypoints.ponits[0];
+    }
+
+    private void Update()
+    {
+        Vector3 direction = target.position - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.5f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    private void GetNextWaypoint()
+    {
+        if (wayPointIndex >= Waypoints.ponits.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        wayPointIndex++;
+        target = Waypoints.ponits[wayPointIndex];
+    }
+}
---
+++ TowerDefense/Assets/Waypoints.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Waypoints : MonoBehaviour {
+
+    public static Transform[] ponits;
+
+    private void Awake()
+    {
+        ponits = new Transform[transform.childCount];
+        for (int i = 0; i < ponits.Length; i++)
+        {
+            ponits[i] = transform.GetChild(i);
+        }
+    }
+}
