a04b3f34255e266bfb0c7d045c2fca50a2d7f5d0
0x0815 <32913299+0x0815@users.noreply.github.com>
2018-06-02 13:04:22 +0200

    Added WaveManager
    Added Wavemanager & small edits on spawner

---
+++ TowerDefense/Assets/WaveSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour {
+
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+    public Text waveCountdownText;
+    public float timeBetweenWaves = 5.5f;
+    private float countdown = 2f;
+    private int waveIndex = 0;
+
+    private void Update()
+    {
+        if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+        waveCountdownText.text = Mathf.Round(countdown).ToString();
+    }
+
+    IEnumerator SpawnWave()
+    {
+        waveIndex++;
+
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            float rndNextSPawn = Random.Range(0.2f,0.5f);
+            yield return new WaitForSeconds(rndNextSPawn);
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
